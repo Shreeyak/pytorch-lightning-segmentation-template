0edd3bb893243bef3f9f8f3525778a819d027b3b
Shreeyak <ss.shrek7@gmail.com>
2020-12-29 15:40:34 +0530

    refactored code, added ability to log every N epochs and N steps

--- seg_lapa/callbacks.py
+++ seg_lapa/callbacks.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 import pytorch_lightning as pl
 import numpy as np
@@ -62,8 +63,23 @@ class EarlyStopping(early_stopping.EarlyStopping):
 
 
 class LogMedia(Callback):
-    def __init__(self, logging_batch_interval: int = 1, max_images_to_log: int = 10):
+    """Logs model output images and other media to weights and biases
+
+    Args:
+        logging_epoch_interval (int): If > 0, log every N epochs. It will extract samples from the first batch.
+        logging_batch_interval (int): If > 0, log every N batches (i.e. steps)
+        max_images_to_log (int): Max number of images to extract from a batch to log.
+    """
+
+    def __init__(
+        self,
+        logging_epoch_interval: int = 1,
+        logging_batch_interval: int = 0,
+        max_images_to_log: int = 10,
+    ):
+
         super().__init__()
+        self.logging_epoch_interval = logging_epoch_interval
         self.logging_batch_interval = logging_batch_interval
         self.max_images_to_log = max_images_to_log
         self.class_labels_lapa = {
@@ -82,44 +98,25 @@ class LogMedia(Callback):
         self.flag_warn_once = False
 
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
-        self._common_batch_end(trainer, outputs, batch, Mode.TRAIN)
+        # Save media
+        if self._should_log(batch_idx, pl_module.current_epoch):
+            self._log_images_to_wandb(trainer, outputs, batch, Mode.TRAIN)
 
     def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
-        self._common_batch_end(trainer, outputs, batch, Mode.VAL)
+        # Save media
+        if self._should_log(batch_idx, pl_module.current_epoch):
+            self._log_images_to_wandb(trainer, outputs, batch, Mode.VAL)
 
     def on_test_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
-        self._common_batch_end(trainer, outputs, batch, Mode.TEST)
+        # Save media
+        if self._should_log(batch_idx, pl_module.current_epoch):
+            self._log_images_to_wandb(trainer, outputs, batch, Mode.TEST)
 
-    def _common_batch_end(self, trainer, outputs, batch, mode: Mode = Mode.TRAIN):
+    def _log_images_to_wandb(self, trainer, outputs, batch, mode: Mode = Mode.TRAIN):
         """Log images to wandb at the end of a batch. Steps are common for train/val/test"""
-        # Log images only every N batches
-        if (trainer.batch_idx + 1) % self.logging_batch_interval != 0:
+        if not self._logger_is_wandb(trainer):
             return
 
-        self._check_for_wandb_logger(trainer)
-
-        mask_list = self._get_wandb_image_from_batch(batch, outputs, mode)
-        wandb_log_label = f"{mode.value}/Predictions"
-        trainer.logger.experiment.log({wandb_log_label: mask_list}, commit=False)
-
-    def _check_for_wandb_logger(self, trainer):
-        """This callback only works with wandb logger.
-        Skip if any other logger detected with warning"""
-        if trainer.logger is None:
-            return None
-        if isinstance(trainer.logger.experiment, pl.loggers.base.DummyExperiment):
-            return None
-        if not isinstance(trainer.logger.experiment, wandb.sdk.wandb_run.Run):
-            if not self.flag_warn_once:
-                # Given warning print only once. To prevent clutter.
-                print(
-                    f"WARN: LogMedia only works with wandb logger. Current logger: {trainer.logger.experiment}. "
-                    f"Will not log any media this run"
-                )
-                self.flag_warn_once = True
-            return None
-
-    def _get_wandb_image_from_batch(self, batch, outputs, mode: Mode = Mode.TRAIN):
         # Pick the batch from GPU0, Dataloader0
         inputs, labels = batch
         if mode == Mode.TRAIN:
@@ -144,4 +141,39 @@ class LogMedia(Callback):
             )
             mask_list.append(mask_img)
 
-        return mask_list
+        wandb_log_label = f"{mode.value}/Predictions"
+        trainer.logger.experiment.log({wandb_log_label: mask_list}, commit=False)
+
+    def _should_log(self, batch_idx: int, epoch_idx: int) -> bool:
+        """Returns True if we should log at this step. We log only every N steps/epochs."""
+        should_continue = False
+
+        if self.logging_epoch_interval > 0:
+            # Only log 1st batch of Nth epoch
+            if ((epoch_idx + 1) % self.logging_epoch_interval == 0) and (batch_idx == 0):
+                should_continue = True
+
+        if self.logging_batch_interval > 0:
+            if (batch_idx + 1) % self.logging_batch_interval == 0:
+                should_continue = True
+
+        return should_continue
+
+    def _logger_is_wandb(self, trainer):
+        """This callback only works with wandb logger.
+        Skip if any other logger detected with warning"""
+        if trainer.logger is None or isinstance(trainer.logger.experiment, pl.loggers.base.DummyExperiment):
+            # DummyExperiment is used in Trainer's initial validation checks
+            return False
+
+        if not isinstance(trainer.logger.experiment, wandb.sdk.wandb_run.Run):
+            if not self.flag_warn_once:
+                # Give warning print only once to prevent clutter.
+                print(
+                    f"WARN: LogMedia only works with wandb logger. Current logger: {trainer.logger.experiment}. "
+                    f"Will not log any media this run"
+                )
+                self.flag_warn_once = True
+            return False
+
+        return True
